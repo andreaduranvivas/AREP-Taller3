c0c78173a02a647208025cdf51429d2bd86e732e
andrea.duran <andrea.duran@mail.escuelaing.edu.co>
2024-02-08 16:12:32 -0500

    Trabajo en Clase Server de Aplicaciones

---
+++ Taller3/src/main/java/arep/clase/myspark/Function.java
@@ -0,0 +1,5 @@
+package arep.clase.myspark;
+
+public interface Function {
+    public String handle(String requestQuery);
+}
---
+++ Taller3/src/main/java/arep/clase/spark/App.java
@@ -0,0 +1,16 @@
+package arep.clase.spark;
+import static spark.Spark.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //get("/hello", (req, resp) -> "Hello Wold!");
+        staticFiles.location("/public"); //http://localhost:4567/ejemplo.html
+        get("/pi", (req, resp) -> Math.PI); //http://localhost:4567/pi
+    }
+}
